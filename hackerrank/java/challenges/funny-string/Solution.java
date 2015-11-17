d0bc4a7a4dec8e35b6e869e390ac86a2bfffae44
Saúl Martínez Vidals <saul.mtz.v@gmail.com>
2015-11-17 00:24:53 -0600

    [HackerRank] Algorithms > Strings > Funny String

---
+++ hackerrank/java/challenges/funny-string/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // test cases
+        int n = in.nextInt();
+
+        // read the words and print the result
+        for (int i = 0; i < n; i++) {
+            boolean funny = true;
+            String word = in.next();
+
+            int l = word.length();
+            for (int j = 0, k = l - 1; j < l - 1; j++, k--) {
+                int diff1 = Math.abs((int) word.charAt(j) - (int) word.charAt(j + 1));
+                int diff2 = Math.abs((int) word.charAt(k) - (int) word.charAt(k - 1));
+
+                if (diff1 != diff2) {
+                    funny = false;
+                    break;
+                }
+            }
+
+            System.out.println(funny ? "Funny" : "Not Funny");
+        }
+    }
+}
+
