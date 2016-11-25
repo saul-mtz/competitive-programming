a80ae6ac5c5fe8c668844c143773f41c5bb7aff6
Saul Martinez Vidals <saul.mtz.v@gmail.com>
2016-11-25 13:54:15 -0600

    Solution.java template

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * @link
+ * @author saul.mtz.v
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+
+}
